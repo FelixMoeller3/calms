571e8e2ee305c9bd885d3e868407ae0141af861d
Felix Möller <felix.moeller4@gmail.com>
2023-01-16 10:15:51 +0100

    Add active learning strategies from Yilin's repo

---
+++ src/active_learning_strategies/__init__.py
@@ -0,0 +1,6 @@
+from .badge import Badge
+from .BALD import BALD
+from .coreset import CoreSet
+from .entropy import Entropy
+from .random_selection import RandomSelection
+from .lc import LC
---
+++ src/active_learning_strategies/lc.py
@@ -0,0 +1,36 @@
+import random
+import torch
+import numpy as np
+import torch.nn.functional as F
+from torch.utils.data import DataLoader
+#custom
+from .strategy import Strategy
+from data.sampler import SubsetSequentialSampler
+
+class LC(Strategy):
+    def __init__(self, model, data_unlabeled, NO_CLASSES, test_loader, cfgs, device):
+        super(LC, self).__init__(model, data_unlabeled, NO_CLASSES, test_loader, cfgs, device)
+
+    def query(self):
+        unlabeled_loader = DataLoader(self.data_unlabeled, batch_size=self.BATCH, 
+                                    sampler=SubsetSequentialSampler(self.subset), 
+                                    pin_memory=True)
+        probs = self.get_predict_prob(unlabeled_loader)
+        probs_max = np.amax(probs,1)
+        arg = np.argsort(-probs_max)
+        return arg
+
+    def get_predict_prob(self, unlabeled_loader):
+        self.model['backbone'].eval()
+        with torch.cuda.device(self.device):
+            predic_probs = torch.tensor([]).cuda()
+
+        with torch.no_grad():
+            for inputs, _, _ in unlabeled_loader:
+                with torch.cuda.device(self.device):
+                    inputs = inputs.cuda()
+                predict, _, _ = self.model['backbone'](inputs)
+                prob = F.softmax(predict, dim=1)
+                predic_probs = torch.cat((predic_probs, prob), 0)
+
+        return predic_probs
