91bae64cfd859be08360bfd2aeb1e73fe8d9dd58
Felix Möller <felix.moeller4@gmail.com>
2023-01-18 17:51:10 +0100

    Start implementing ewc and continue implementing mas

---
+++ src/continual_learning_strategies/cl_base.py
@@ -0,0 +1,22 @@
+from abc import ABC, abstractmethod
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+
+class ContinualLearningStrategy(ABC):
+    '''
+        This is the base class for all continual 
+    '''
+    def __init__(self, model:nn.Module,crit,lr:float=0.001):
+        self.model = model
+        pass
+
+    @abstractmethod
+    def train(self, dataloader: DataLoader):
+        pass
+
+    def save(self, filename: str):
+        torch.save(self.model, filename)
+
+    def load(self, filename: str):
+        self.model = torch.load(filename)
