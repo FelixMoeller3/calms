457e8c4ee55b78e870481eeb454944f1de49cf73
Felix Möller <felix.moeller4@gmail.com>
2023-02-02 17:17:06 +0100

    Finish own functioning version of EWC

--- src/continual_learning_strategies/ewc.py
+++ src/continual_learning_strategies/ewc.py
@@ -1,13 +1,11 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-from avalanche.training import EWC
-from avalanche.benchmarks import dataset_benchmark
-from torch import autograd
 from torch.utils.data import DataLoader,Dataset
 from .cl_base import ContinualLearningStrategy
-from torchvision.transforms import Compose
-from typing import List
+import time
+import random
+from torch.nn import functional as F
+from tqdm import tqdm
 
 
 class ElasticWeightConsolidation(ContinualLearningStrategy):
@@ -19,43 +17,46 @@ class ElasticWeightConsolidation(ContinualLearningStrategy):
         https://github.com/thuyngch/Overcoming-Catastrophic-Forgetting
     '''
 
-    def __init__(self,model:nn.Module,optim: torch.optim.Optimizer,crit: nn.CrossEntropyLoss,weight:int=100000):
+    def __init__(self,model:nn.Module,optim: torch.optim.Optimizer,crit: nn.CrossEntropyLoss,weight:float=1.0):
         self.model = model
         self.weight = weight
         self.crit = crit
         self.optimizer = optim
-        self.ewc = EWC(self.model,self.optimizer,self.crit,self.weight,'separate')
+        self.prev_params = {}
+        self._save_model_params()
+        self.fisher = {}
+        self._update_fisher_params()
 
-    def train(self, train_datasets: List[Dataset], test_datasets: List[Dataset], num_epochs: int, batch_size:int=100 ,transform: Compose=None):
+    def _save_model_params(self) -> None:
+        '''
+            Saves the current parameters of the model before training on a new task because the old 
+            model parameters are needed to compute the loss function
+        '''
+        for name,param in self.model.named_parameters():
+            self.prev_params[name] = param.detach().clone()
+
+    def train(self, dataloaders: dict[str,DataLoader], num_epochs: int):
         '''
             Trains the model for num_epoch epochs using the dataloaders 'train' and 'val' in the dataloaders dict
-        '''
-        self.ewc.train_epochs = num_epochs
-        self.ewc.train_mb_size = batch_size
-        self.ewc.eval_mb_size = batch_size
-        scenario = dataset_benchmark(train_datasets,test_datasets,train_transform=transform,eval_transform=transform)
-        self.ewc.train(scenario.train_stream)
-        self.ewc.eval(scenario.test_stream)
         '''
         start_time = time.time()
         for epoch in range(num_epochs):
             print(f"Running epoch {epoch+1}/{num_epochs}")
-            self.ewc.train()
             self._run_train_epoch(dataloaders['train'])
             self._run_val_epoch(dataloaders['val'])
+        self._save_model_params()
+        self._update_fisher_params(dataloaders['train'].dataset,0.05)
         time_elapsed = time.time() - start_time
-        print('Training complete in {:.0f}m {:.0f}s'.format(
-        time_elapsed // 60, time_elapsed % 60))
-        '''
+        print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
 
-    def _run_train_epoch(self,dataloader: DataLoader,dataset_size: int) -> None:
+    def _run_train_epoch(self,dataloader: DataLoader) -> None:
         '''
             Runs one epoch of the training procedure with the data given by the dataloader.
         '''
         self.model.train(True)
         total_loss = 0.0
         correct_predictions = 0
-        for data in dataloader:
+        for data in tqdm(dataloader):
 
             inputs, labels = data
 
@@ -63,26 +64,25 @@ class ElasticWeightConsolidation(ContinualLearningStrategy):
             
             outputs = self.model(inputs)
             loss = self._compute_consolidation_loss() + self.crit(outputs, labels)
-            self.optimizer.zero_grad()
             loss.backward()
             self.optimizer.step()
             _, preds = torch.max(outputs.data, 1)
             total_loss += loss.item()
             correct_predictions += torch.sum(preds == labels.data).item()
         
-        epoch_loss = total_loss / dataset_size
-        epoch_acc = correct_predictions / dataset_size
+        epoch_loss = total_loss / len(dataloader.dataset)
+        epoch_acc = correct_predictions / len(dataloader.dataset)
 
         print('Training Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
 
-    def _run_val_epoch(self,dataloader: DataLoader, dataset_size: int):
+    def _run_val_epoch(self,dataloader: DataLoader):
         '''
             Runs one validation epoch using the dataloader which contains the validation data. 
         '''
         total_loss = 0.0
         correct_predictions = 0
         self.model.train(False)
-        for data in dataloader:
+        for data in tqdm(dataloader):
             inputs, labels = data
 
             outputs = self.model(inputs)
@@ -91,69 +91,43 @@ class ElasticWeightConsolidation(ContinualLearningStrategy):
             total_loss += loss.item()
             correct_predictions += torch.sum(preds == labels.data).item()
 
-        epoch_loss = total_loss / dataset_size
-        epoch_acc = correct_predictions / dataset_size
+        epoch_loss = total_loss / len(dataloader.dataset)
+        epoch_acc = correct_predictions / len(dataloader.dataset)
         
         print('Validation Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))
 
-    def _update_mean_params(self):
+
+    def _update_fisher_params(self, train_dataset: Dataset=None, sample_size:float=0.5):
         '''
             TODO: Add method description
         '''
-        for param_name, param in self.model.named_parameters():
-            _buff_param_name = param_name.replace('.', '__')
-            self.model.register_buffer(_buff_param_name+'_estimated_mean', param.data.clone())
+        num_samples = (len(train_dataset) * sample_size) if train_dataset else 0
+        self.fisher = {}
+        for name,param in self.model.named_parameters():
+            self.fisher[name] = torch.zeros_like(param)
+        for _ in range(int(num_samples)):
+            cur_index = random.randint(0,len(train_dataset)-1)
+            elem, label = train_dataset[cur_index]
+            self.optimizer.zero_grad()
+            output = self.model(elem)
+            sm = F.log_softmax(output,dim=1)
+            label_tensor = torch.tensor([label],dtype=torch.long)
+            loss = F.nll_loss(sm,label_tensor)
+            loss.backward()
 
-    def _update_fisher_params(self, dataloader: DataLoader):
-        '''
-            TODO: Add method description
-        '''
-        log_likelihoods = []
-        for i, (input, target) in enumerate(dataloader):
-            input = input.view(input.shape[0],-1)
-            if i> 0 and input.shape[0] != log_likelihoods[0].shape[0]:
-                break
-            output = F.log_softmax(self.model(input), dim=1)
-            log_likelihoods.append(output[:, target])
-        # TODO: fix calculation in line below 
-        # (see https://github.com/shivamsaboo17/Overcoming-Catastrophic-forgetting-in-Neural-Networks/issues/6)
-        # for more details
-        log_likelihood = torch.cat(log_likelihoods).mean()
-        grad_log_liklihood = autograd.grad(log_likelihood, self.model.parameters())
-        _buff_param_names = [param[0].replace('.', '__') for param in self.model.named_parameters()]
-        for _buff_param_name, param in zip(_buff_param_names, grad_log_liklihood):
-            self.model.register_buffer(_buff_param_name+'_estimated_fisher', param.data.clone() ** 2)
+            for name, param in self.model.named_parameters():
+                self.fisher[name] += param.grad.data ** 2 / num_samples
 
-    def register_ewc_params(self, dataloader: DataLoader):
-        self._update_fisher_params(dataloader)
-        self._update_mean_params()
 
     def _compute_consolidation_loss(self):
         '''
             TODO: Add method description
         '''
-        try:
-            losses = []
-            for param_name, param in self.model.named_parameters():
-                _buff_param_name = param_name.replace('.', '__')
-                estimated_mean = getattr(self.model, '{}_estimated_mean'.format(_buff_param_name))
-                estimated_fisher = getattr(self.model, '{}_estimated_fisher'.format(_buff_param_name))
-                losses.append((estimated_fisher * (param - estimated_mean) ** 2).sum())
-            return (self.weight / 2) * sum(losses)
-        except AttributeError:
-            return 0
-
-    def forward_backward_update(self, input, target):
-        '''
-            TODO: Add method description
-        '''
-        output = self.model(input)
-        loss = self._compute_consolidation_loss() + self.crit(output, target)
-        self.optimizer.zero_grad()
-        loss.backward()
-        self.optimizer.step()
-    
-
-    def eval(self,evalset: Dataset, transform: Compose=None):
-        scenario = dataset_benchmark([],[evalset],train_transform=transform,eval_transform=transform)
-        self.ewc.eval(scenario.test_stream[0])
+        loss = torch.tensor(0.0)
+        for name,param in self.model.named_parameters():
+            diff:torch.Tensor = param - self.prev_params[name]
+            diff.pow_(2)
+            diff.mul_(self.fisher[name])
+            loss += diff.sum()
+        return loss * (self.weight / 2)
+        
