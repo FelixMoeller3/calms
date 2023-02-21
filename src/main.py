ef76df593ccaf5eeaad044aedfb8d44eb0b2e814
ie2651@partner.kit.edu <ie2651@uc2n996.localdomain>
2023-02-21 13:33:59 +0100

    Modified job file and requirements

--- src/main.py
+++ src/main.py
@@ -5,13 +5,14 @@ from utils import config
 from torchvision import datasets,transforms
 
 #parser = argparse.ArgumentParser(description='Run experiment for model stealing')
-al = ["Random","BALD", "LC"]
-cl = ["MAS","Naive", "EWC", "IMM", "Alasso"]
-for a in al:
-    for c in cl:
-        config.run_cl_al_config("./src/conf/basic_model_stealing/" + a + "_" + c + ".yaml")
+#al = ["Random","BALD", "LC"]
+#cl = ["MAS","Naive", "EWC", "IMM", "Alasso"]
+#for a in al:
+#    for c in cl:
+#        config.run_cl_al_config("./src/conf/basic_model_stealing/" + a + "_" + c + ".yaml")
 #for c in cl:
 #    config.run_cl_al_config("./src/conf/basic_model_stealing/" + "LC" + "_" + c + ".yaml")
 #for a in al:
 #    for c in cl:
 #        config.run_cl_al_config("./src/conf/basic_model_stealing/" + a + "_" + c + ".yaml")
+print("hello world")
