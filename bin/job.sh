a85f3aa1656e5ff60ef9484b92aa5c11f570fad7
Felix Moeller <felix.moeller4@gmail.com>
2023-02-27 14:39:16 +0100

    Run active learning experiment

--- bin/job.sh
+++ bin/job.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=AL_Baseline       # job name
 #SBATCH --partition=gpu_4                  # queue for the resource allocation.
-#SBATCH --time=30:00                     # wall-clock time limit  
+#SBATCH --time=100:00                     # wall-clock time limit  
 #SBATCH --mem=10000                        # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=40                 # number of CPUs required per MPI task
@@ -16,15 +16,15 @@ module load devel/cuda/11.8
 source ../ba_env/bin/activate   
 configs=(
 	#"./src/conf/finetuning/Finetune_MAS.yaml"
-	#"./src/conf/basic_model_stealing/LC_EWC.yaml"
-       #"./src/conf/basic_model_stealing/BALD_Naive.yaml"
-        #"./src/conf/basic_model_stealing/CoreSet_Naive.yaml"
-        #"./src/conf/basic_model_stealing/Badge_Naive.yaml"
 	"./src/conf/basic_model_stealing/LC_Naive.yaml"
+        "./src/conf/basic_model_stealing/Random_Naive.yaml"
+	"./src/conf/basic_model_stealing/BALD_Naive.yaml"
+        "./src/conf/basic_model_stealing/CoreSet_Naive.yaml"
+        "./src/conf/basic_model_stealing/Badge_Naive.yaml"
 )
 for conf in "${configs[@]}"
 do 
-    echo "Running $conf with mode CL"
-    python ./src/main.py -c $conf -m "CL"
+    echo "Running $conf with mode AL"
+    python ./src/main.py -c $conf -m "AL"
 done
 deactivate
