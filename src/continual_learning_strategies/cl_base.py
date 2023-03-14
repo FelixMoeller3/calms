Enable training with softmax labels and fix errors in model stealing workflow

src/continual_learning_strategies/cl_base.py
@@ -66,6 +66,11 @@ class ContinualLearningStrategy(ABC):
             # Stop updating the regularization params during training
             #self._update_reg_params(outputs,labels.size(0))
             _, preds = torch.max(outputs.data, 1)
+            # The labels might be softmax labels, therefore here the class label is computed
+            if labels.dim() > 1:
+                _, class_labels = torch.max(labels.data, 1)
+            else:
+                class_labels = labels
             loss = self.crit(outputs, labels) + self._compute_regularization_loss()
 
             loss.backward()
@@ -73,7 +78,7 @@ class ContinualLearningStrategy(ABC):
                 clip_grad.clip_grad_norm_(self.model.parameters(),self.clip_grad)
             self.optim.step()
             total_loss += loss.item()
-            correct_predictions += torch.sum(preds == labels.data).item()
+            correct_predictions += torch.sum(preds == class_labels.data).item()
         epoch_loss = total_loss / len(train_loader.dataset)
         epoch_acc = correct_predictions / len(train_loader.dataset)
         print('Training Loss: {:.4f} Acc: {:.4f}'.format(epoch_loss, epoch_acc))

src/continual_learning_strategies/ewc.py
@@ -41,6 +41,7 @@ class ElasticWeightConsolidation(ContinualLearningStrategy):
         '''
             TODO: Add method description
         '''
+        # TODO: Fix so we can work with class label when having softmax label
         num_samples = (len(train_dataset) * sample_size) if train_dataset else 0
         self.fisher = {}
         for name,param in self.model.named_parameters():
@@ -48,13 +49,18 @@ class ElasticWeightConsolidation(ContinualLearningStrategy):
         for _ in range(int(num_samples)):
             cur_index = random.randint(0,len(train_dataset)-1)
             elem, label = train_dataset[cur_index]
+            # Labels can be an array when they are the softmax output of the target model
+            if isinstance(label,torch.Tensor):
+                class_label = torch.argmax(label).item()
+            else:
+                class_label = label
             self.optim.zero_grad()
             input = torch.unsqueeze(elem,0)
             if self.use_gpu:
                 input = input.cuda()
             output = self.model(input)
             sm = F.log_softmax(output,dim=1)
-            label_tensor = torch.tensor([label],dtype=torch.long).cuda() if self.use_gpu else torch.tensor([label],dtype=torch.long)
+            label_tensor = torch.tensor([class_label],dtype=torch.long).cuda() if self.use_gpu else torch.tensor([class_label],dtype=torch.long)
             loss = F.nll_loss(sm,label_tensor)
             loss.backward()
 

src/continual_learning_strategies/imm.py
@@ -110,10 +110,14 @@ class IMM(ContinualLearningStrategy):
                 elem = torch.tensor(elem)
             if self.use_gpu:
                 elem = elem.cuda()
+            if isinstance(label,torch.Tensor):
+                class_label = torch.argmax(label).item()
+            else:
+                class_label = label
             self.optim.zero_grad()
             output = self.model(elem)
             sm = F.log_softmax(output,dim=1)
-            label_tensor = torch.tensor([label],dtype=torch.long).cuda() if self.use_gpu else torch.tensor([label],dtype=torch.long)
+            label_tensor = torch.tensor([class_label],dtype=torch.long).cuda() if self.use_gpu else torch.tensor([class_label],dtype=torch.long)
             loss = F.nll_loss(sm,label_tensor)
             loss.backward()
 

src/foo.py
@@ -6,6 +6,7 @@ import torch.nn as nn
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import process_runner as ms
+import torch
 num_cycles = 10
 init_budget = 5000
 cycle_budget = 1000
@@ -80,4 +81,4 @@ with open("data/experiments/results.txt",'a') as f:
 
 if __name__ == "__main__":
     from utils import config
-    config.run_cl_al_config("./src/conf/Example_conf.yaml")
+    config.run_config("./src/conf/Example_conf.yaml")

src/process_runner/msprocess.py
@@ -6,10 +6,12 @@ from continual_learning_strategies.cl_base import ContinualLearningStrategy
 from torch.utils.data import Dataset,DataLoader,Subset
 import random
 from .process_base import BaseProcess
+from data import Softmax_label_set
+
 
 class ModelStealingProcess(BaseProcess):
 
-    def __init__(self,targetModel:nn.Module,activeLearningStrategy: Strategy,continualLearningStrategy: ContinualLearningStrategy,state_dir:str=None):
+    def __init__(self,targetModel:nn.Module,activeLearningStrategy: Strategy,continualLearningStrategy: ContinualLearningStrategy,state_dir:str=None,use_gpu:bool=False):
         '''
             :param targetModel: the target model in the model stealing process (i.e. the one that will be stolen). Needs to be pretrained!!
             :param activeLearningStrategy: the active learning strategy to use in the model stealing process.
@@ -19,6 +21,7 @@ class ModelStealingProcess(BaseProcess):
         super(ModelStealingProcess,self).__init__(activeLearningStrategy,continualLearningStrategy,state_dir)
         self.target_model = targetModel
         self.substitute_model = self.cl_strat.model
+        self.use_gpu = use_gpu
 
     def steal_model(self,train_set: Dataset,val_set: Dataset,batch_size:int,num_cycles:int,num_epochs:int,use_label:bool=True,
                     start_cycle:int=0,labeled_set:List[int]=[],unlabeled_set:List[int]=[],val_accuracies:List[float]=[],
@@ -35,6 +38,8 @@ class ModelStealingProcess(BaseProcess):
         '''
         # set all labels to -1 to ensure no labels are given before
         #train_set.targets[train_set.targets > -1] = -1
+        if not use_label:
+            train_set = Softmax_label_set(train_set)
         val_loader = DataLoader(val_set,batch_size,shuffle=True)
         loaders_dict = {'train': None, 'val': val_loader}
         if start_cycle == 0:
@@ -58,27 +63,36 @@ class ModelStealingProcess(BaseProcess):
             labeled_set += training_examples_absolute_indices
             unlabeled_set = [i for i in unlabeled_set if i not in training_examples_absolute_indices]
             self._train_cycle(train_set,training_examples_absolute_indices,loaders_dict,batch_size,num_epochs,val_accuracies)
-            
+            cur_agreement = self._compute_agreement(loaders_dict['val'])
+            print("Current agreement is: {:.4f}".format(cur_agreement))
+            agreements.append(cur_agreement)
             if self.state_dir is not None:
                 state_dict = {'start_cycle': i+1, 'labeled_set': labeled_set, 'unlabeled_set': unlabeled_set,'val_accuracies': val_accuracies, 'agreements': agreements}
                 self._save_state(state_dict)
         return val_accuracies,agreements
     
-    def _add_targets(self, train_set: Dataset,labels_to_add: List[int],use_label:bool=True) -> None:
+    def _add_targets(self, train_set: Dataset,labels_to_add: List[int],use_label:bool) -> None:
         '''
             Adds the respective targets to the dataset. The targets are determined by querying the target model.
         '''
-        if use_label:
-            for index in labels_to_add:
-                train_set.targets[index] = torch.max(self.target_model(torch.unsqueeze(train_set[index][0],0)),1)[1]
-        else:
-            for index in labels_to_add:
-                train_set.targets[index] = torch.nn.functional.softmax(self.target_model(torch.unsqueeze(train_set[index][0],0)),dim=0)
-
+        # TODO: Adapt to gpu usage
+        with torch.no_grad():
+            if use_label:
+                for index in labels_to_add:
+                    cur_elem = train_set[index][0].cuda() if self.use_gpu else train_set[index][0]
+                    train_set.targets[index] = torch.max(self.target_model(torch.unsqueeze(cur_elem,0)),1)[1]
+            else:
+                for index in labels_to_add:
+                    cur_elem = train_set[index][0].cuda() if self.use_gpu else train_set[index][0]
+                    current_example = torch.unsqueeze(cur_elem,0)
+                    output = torch.squeeze(self.target_model(current_example))
+                    softmax_res = torch.nn.functional.softmax(output,dim=0)
+                    train_set.targets[index] = softmax_res
     
     def _compute_agreement(self, val_loader: DataLoader) -> None:
         agreed_predictions = 0
         for inputs,_ in val_loader:
+            inputs = inputs.cuda() if self.use_gpu else inputs
             _, target_model_preds = torch.max(self.target_model(inputs).data,1)
             _, substitute_model_preds = torch.max(self.cl_strat.model(inputs).data,1)
             agreed_predictions += torch.sum(target_model_preds == substitute_model_preds).item()