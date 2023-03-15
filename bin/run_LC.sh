cac17ee1a04d2fea2a4f28c5931b77ec6589a7d5
Felix Moeller <felix.moeller4@gmail.com>
2023-03-15 14:12:29 +0100

    Train new target models

--- bin/run_LC.sh
+++ bin/run_LC.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=LC_Run       # job name
 #SBATCH --partition=gpu_4                  # queue for the resource allocation.
-#SBATCH --time=600:00                     # wall-clock time limit  
+#SBATCH --time=30:00                     # wall-clock time limit  
 #SBATCH --mem=10000                        # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
 #SBATCH --cpus-per-task=2                 # number of CPUs required per MPI task
