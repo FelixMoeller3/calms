01c59ab19de03e3ede4ded12de0d016fea5ce5f6
Felix Moeller <felix.moeller4@gmail.com>
2023-03-17 22:35:34 +0100

    Run new experiments scheduling with IMM and EWC

--- bin/run_CoreSet_EWC.sh
+++ bin/run_CoreSet_EWC.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name=CoreSet_EWC       # job name
 #SBATCH --partition=gpu_4_a100             # queue for the resource allocation.
 #SBATCH --time=600:00                     # wall-clock time limit  
-#SBATCH --mem=10000                        # memory per node
+#SBATCH --mem=20000                        # memory per node
 #SBATCH --nodes=1                          # number of nodes to be used
-#SBATCH --cpus-per-task=4                 # number of CPUs required per MPI task
+#SBATCH --cpus-per-task=2                 # number of CPUs required per MPI task
 #SBATCH --ntasks-per-node=1                # maximum count of tasks per node
 #SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
 #SBATCH --mail-user=ie2651@partner.kit.edu # notification email address
