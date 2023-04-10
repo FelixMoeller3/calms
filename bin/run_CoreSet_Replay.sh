c28e320219f3ade0ee3672c304c96bbfdffe5d64
Felix Moeller <felix.moeller4@gmail.com>
2023-04-10 19:11:50 +0200

    Finish Active Thief experiments

---
+++ bin/run_CoreSet_Replay.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+#SBATCH --job-name=CoreSet_Replay       # job name
+#SBATCH --partition=gpu_4                  # queue for the resource allocation.
+#SBATCH --time=1000:00                     # wall-clock time limit  
+#SBATCH --mem=10000                        # memory per node
+#SBATCH --nodes=1                          # number of nodes to be used
+#SBATCH --cpus-per-task=2                 # number of CPUs required per MPI task
+#SBATCH --ntasks-per-node=1                # maximum count of tasks per node
+#SBATCH --mail-type=ALL                    # Notify user by email when certain event types occur.
+#SBATCH --mail-user=ie2651@partner.kit.edu # notification email address
+#SBATCH --gres=gpu:1
+
+module purge                                       # Unload all currently loaded modules.
+module load devel/cuda/11.8
+source ../ba_env/bin/activate   
+configs=(
+	#"./src/conf/finetuning/Finetune_MAS.yaml"
+	"./src/conf/basic_model_stealing/CoreSet_Replay.yaml"
+        #"./src/conf/basic_model_stealing/Random_Naive.yaml"
+	#"./src/conf/basic_model_stealing/BALD_Naive.yaml"
+        #"./src/conf/basic_model_stealing/CoreSet_Naive.yaml"
+        #"./src/conf/basic_model_stealing/Badge_Naive.yaml"
+)
+for conf in "${configs[@]}"
+do 
+    echo "Running $conf with mode CL"
+    python ./src/main.py -c $conf -m "CL"
+done
+deactivate
